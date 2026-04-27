3f52cc190d8d8621cd8eb6c681584d58557935db
lalithakolla@gmail.com <lalithakolla@gmail.com>
2026-04-26 22:04:57 -0400

    add updated code

--- index.js
+++ index.js
@@ -133,13 +133,9 @@ const nameSorter = document.querySelector("#name-sorter");
 
 // Add an event listener
 nameSorter.addEventListener("click", (event) => {
-  console.log("You clicked the name sorter");
-});
-// preventDefault() method of the event object:
-nameSorter.addEventListener("click", (event) => {
   event.preventDefault();
-//   console.log("You clicked the name sorter");
-  // 1.  Get the main element
+
+  // 1. Get the main element
   const main = document.querySelector("main");
 
   // 2. Get the list of parks
@@ -147,4 +143,25 @@ nameSorter.addEventListener("click", (event) => {
 
   // 3. Empty the main element
   main.innerHTML = "";
+
+  // 4. Create an array
+  const parksArray = Array.from(parksList);
+
+  // 5. Sort the array
+  parksArray.sort((parkA, parkB) => {
+    const parkAName = parkA.querySelector("h2").innerText;
+    const parkBName = parkB.querySelector("h2").innerText;
+    if (parkAName < parkBName) {
+      return -1;
+    } else if (parkAName > parkBName) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+
+  // 6. Insert each park into the DOM
+  parksArray.forEach((park) => {
+    main.appendChild(park);
+  });
 });
