07f3aaa2fe5a05a8c6d876cdb8fe8dc6ca557148
lalithakolla@gmail.com <lalithakolla@gmail.com>
2026-04-26 22:08:12 -0400

    add updated code

--- index.js
+++ index.js
@@ -127,41 +127,87 @@ allBtns.forEach((btn) => {
     park.style.backgroundColor = "#c8e6c9";
   });
 });
+ 
+// // 1st method
+// // Select the `nameSorter` link
+// const nameSorter = document.querySelector("#name-sorter");
 
-// Select the `nameSorter` link
-const nameSorter = document.querySelector("#name-sorter");
+// // Add an event listener
+// nameSorter.addEventListener("click", (event) => {
+//   event.preventDefault();
 
-// Add an event listener
-nameSorter.addEventListener("click", (event) => {
+//   // 1. Get the main element
+//   const main = document.querySelector("main");
+
+//   // 2. Get the list of parks
+//   const parksList = main.querySelectorAll(".park-display");
+
+//   // 3. Empty the main element
+//   main.innerHTML = "";
+
+//   // 4. Create an array
+//   const parksArray = Array.from(parksList);
+
+//   // 5. Sort the array
+//   parksArray.sort((parkA, parkB) => {
+//     const parkAName = parkA.querySelector("h2").innerText;
+//     const parkBName = parkB.querySelector("h2").innerText;
+//     if (parkAName < parkBName) {
+//       return -1;
+//     } else if (parkAName > parkBName) {
+//       return 1;
+//     } else {
+//       return 0;
+//     }
+//   });
+
+//   // 6. Insert each park into the DOM
+//   parksArray.forEach((park) => {
+//     main.appendChild(park);
+//   });
+// });
+
+// 2nd Method:
+// Function for sorting by name
+const sortByName = (parkA, parkB) => {
+  const parkAName = parkA.querySelector("h2").innerText;
+  const parkBName = parkB.querySelector("h2").innerText;
+  if (parkAName < parkBName) {
+    return -1;
+  } else if (parkAName > parkBName) {
+    return 1;
+  } else {
+    return 0;
+  }
+};
+
+// Function for handling the `nameSorter` click
+const nameSorterClickHandler = (event) => {
   event.preventDefault();
 
-  // 1. Get the main element
+  // 1.  Get the main element
   const main = document.querySelector("main");
 
   // 2. Get the list of parks
   const parksList = main.querySelectorAll(".park-display");
 
-  // 3. Empty the main element
+  // 3. Empty the main
   main.innerHTML = "";
 
   // 4. Create an array
   const parksArray = Array.from(parksList);
 
   // 5. Sort the array
-  parksArray.sort((parkA, parkB) => {
-    const parkAName = parkA.querySelector("h2").innerText;
-    const parkBName = parkB.querySelector("h2").innerText;
-    if (parkAName < parkBName) {
-      return -1;
-    } else if (parkAName > parkBName) {
-      return 1;
-    } else {
-      return 0;
-    }
-  });
+  parksArray.sort(sortByName);
 
   // 6. Insert each park into the DOM
   parksArray.forEach((park) => {
     main.appendChild(park);
   });
-});
+};
+
+// Select the `nameSorter` link
+const nameSorter = document.querySelector("#name-sorter");
+
+// Add an event listener
+nameSorter.addEventListener("click", nameSorterClickHandler);
